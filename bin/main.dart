ce361ba0e0dae3331e316b8a9083683b01aa797e
Fernando Muñoz <fernandrewm@gmail.com>
2021-02-21 13:47:56 -0600

    Starting to consume the API

--- bin/main.dart
+++ bin/main.dart
@@ -6,6 +6,11 @@ void main(List<String> arguments) {
   final url = 'https://reqres.in/api/users?page=2';
 
   http.get(url).then((resp) {
-    print(resp);
+    // print(resp);
+    final body = jsonDecode(resp.body);
+    // print(body);
+    print('Page: ${body['page']}');
+    print('per_page: ${body['per_page']}');
+    print('id del tercer elemento: ${body['data'][2]['id']}');
   });
 }
