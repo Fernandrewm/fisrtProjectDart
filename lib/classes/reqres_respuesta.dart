d8533279cc35091ff16b03271fe952a571a9e362
Fernando Muñoz <fernandrewm@gmail.com>
2021-02-21 15:01:51 -0600

    Optimizing code by separating content by classes

--- lib/classes/reqres_respuesta.dart
+++ lib/classes/reqres_respuesta.dart
@@ -6,6 +6,8 @@
 
 import 'dart:convert';
 
+import 'package:paquetes/classes/persona.dart';
+
 ReqResRespuesta reqResRespuestaFromJson(String str) =>
     ReqResRespuesta.fromJson(json.decode(str));
 
@@ -46,38 +48,6 @@ class ReqResRespuesta {
         "total_pages": totalPages,
         "data": List<dynamic>.from(data.map((x) => x.toJson())),
         "support": support.toJson(),
-      };
-}
-
-class Persona {
-  Persona({
-    this.id,
-    this.email,
-    this.firstName,
-    this.lastName,
-    this.avatar,
-  });
-
-  int id;
-  String email;
-  String firstName;
-  String lastName;
-  String avatar;
-
-  factory Persona.fromJson(Map<String, dynamic> json) => Persona(
-        id: json["id"],
-        email: json["email"],
-        firstName: json["first_name"],
-        lastName: json["last_name"],
-        avatar: json["avatar"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "email": email,
-        "first_name": firstName,
-        "last_name": lastName,
-        "avatar": avatar,
       };
 }
 
--- lib/paquetes.dart
+++ lib/paquetes.dart
@@ -1,1 +1,21 @@
+import 'package:http/http.dart' as http;
+import 'package:paquetes/classes/reqres_respuesta.dart';
+// import 'classes/reqres_respuesta.dart';
 
+void getReqResp_service() {
+  final url = 'https://reqres.in/api/users?page=2';
+
+  http.get(url).then((resp) {
+    final resReqRes = reqResRespuestaFromJson(resp.body);
+    // print(resp);
+    // final body = jsonDecode(resp.body);
+    // print(body);
+    // print('Page: ${body['page']}');
+    // print('per_page: ${body['per_page']}');
+    // print('id del tercer elemento: ${body['data'][2]['id']}');
+
+    print('Page: ${resReqRes.page}');
+    print('per_page: ${resReqRes.perPage}');
+    print('id del tercer elemento: ${resReqRes.data[2].id}');
+  });
+}
