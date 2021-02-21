8df88506418ba15428f5a995b5d89dd764999e36
Fernando Muñoz <fernandrewm@gmail.com>
2021-02-21 13:38:48 -0600

    Establishing API connection via HTTP

---
+++ bin/main.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:paquetes/paquetes.dart' as paquetes;
+
+void main(List<String> arguments) {
+  final url = 'https://reqres.in/api/users?page=2';
+
+  http.get(url).then((resp) {
+    print(resp);
+  });
+}
